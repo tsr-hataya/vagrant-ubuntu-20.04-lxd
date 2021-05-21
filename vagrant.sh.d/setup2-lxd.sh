192ec0cbb28f99c1532411c020e82e1de80b8baf
hataya <hataya@tsr-company.com>
2021-05-21 16:41:29 +0900

    lxdbr0を設定する処理を追加 ”netplan apply"を省略することでSSH接続が切断されるのを回避

--- vagrant.sh.d/setup2-lxd.sh
+++ vagrant.sh.d/setup2-lxd.sh
@@ -17,17 +17,22 @@ echo "==> [TASK2] Add a bridge interface."
 
 # 変数をセット
 
+NIC0=`ip address show | grep "inet 10.." |  awk '{print $8}'`
+echo "NIC0=${NIC0}"
+
 NIC1=`ip address show | grep "inet 192.168.56." |  awk '{print $7}'`
 echo "NIC1=${NIC1}"
 
 IPADDR1=`ip address show | grep "inet 192.168.56." |  awk '{print $2}'`
 echo "IPADDR1=${IPADDR1}"
 
+mv /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml-`date '+%Y%m%d-%H%M%S'`
 mv /etc/netplan/50-vagrant.yaml /etc/netplan/50-vagrant.yaml-`date '+%Y%m%d-%H%M%S'`
+cp /vagrant.conf.d/50-lxdbr0.yaml /etc/netplan/50-lxdbr0.yaml
+sed -i "s|==NIC0==|${NIC0}|g" /etc/netplan/50-lxdbr0.yaml
 cp /vagrant.conf.d/50-lxdbr1.yaml /etc/netplan/50-lxdbr1.yaml
 sed -i "s|==NIC1==|${NIC1}|g" /etc/netplan/50-lxdbr1.yaml
 sed -i "s|==IPADDR1==|${IPADDR1}|g" /etc/netplan/50-lxdbr1.yaml
-netplan apply
 echo ""
 
 
