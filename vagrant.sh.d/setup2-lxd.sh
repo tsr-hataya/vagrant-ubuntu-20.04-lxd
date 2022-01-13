e425b55a1b9c09af09a3e4c56907351967feb2ce
tsr-hataya <hataya@tsr-company.com>
2022-01-13 14:57:02 +0900

    変数をセットする処理を修正

--- vagrant.sh.d/setup2-lxd.sh
+++ vagrant.sh.d/setup2-lxd.sh
@@ -20,7 +20,7 @@ echo "==> [TASK2] Add a bridge interface."
 NIC0=`ip address show | grep "inet 10." |  awk '{print $8}'`
 echo "NIC0=${NIC0}"
 
-NIC1=`ip address show | grep "inet 192.168.56." |  awk '{print $8}'`
+NIC1=`ip address show | grep "inet 192.168.56." |  awk '{print $7}'`
 echo "NIC1=${NIC1}"
 
 IPADDR1=`ip address show | grep "inet 192.168.56." |  awk '{print $2}'`
